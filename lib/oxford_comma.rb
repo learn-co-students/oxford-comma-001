3b75900426ffae020c16e5d30569f422488ed57e
Chris Aporta <christopher.aporta@gmail.com>
2015-08-27 21:49:24 -0400

    refactor lab

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,6 @@
 def oxford_comma(arr)
   if arr.size > 2
-    last_val = arr.pop.prepend("and ")
-    arr.push(last_val)
+    arr.last.prepend("and ")
     arr.join(", ")
   else
     arr.join(" and ")
