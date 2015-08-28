0b163e5e6442d59c60ed7752463f04dcb6ba93c1
Chris Aporta <christopher.aporta@gmail.com>
2015-08-27 21:47:10 -0400

    complete lab

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
-def oxford_comma(array)
-
+def oxford_comma(arr)
+  if arr.size > 2
+    last_val = arr.pop.prepend("and ")
+    arr.push(last_val)
+    arr.join(", ")
+  else
+    arr.join(" and ")
+  end
 end
