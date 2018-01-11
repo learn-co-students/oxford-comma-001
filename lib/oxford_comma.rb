73b72a2bc220827a6875dcbddbc7242556667d8c
andrewkjankowski <andrewkjankowski@gmail.com>
2018-01-11 16:31:24 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    a = "and " << array.pop
+    array.push(a)
+    array.join(', ')
+  end
 end
